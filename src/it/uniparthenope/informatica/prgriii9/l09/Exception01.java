bf617ea69f2a00ff55c052fd91c6eae246d059af
angel <angelo.marzocchi001@studenti.uniparthenope.it>
2021-11-11 14:53:50 +0100

    Implemented main with a strict policy to string/double conversion

--- src/it/uniparthenope/informatica/prgriii9/l09/Exception01.java
+++ src/it/uniparthenope/informatica/prgriii9/l09/Exception01.java
@@ -2,6 +2,14 @@ package it.uniparthenope.informatica.prgriii9.l09;
 
 public class Exception01 {
     public static void main(String[] args) {
-
+        try {
+            double sum = 0.0;
+            for (String ars : args) {
+                sum += Double.parseDouble(ars);
+            }
+            System.out.println("sum =" + sum);
+        } catch(NumberFormatException e){
+            System.out.println(e.getMessage());
+        }
     }
 }
